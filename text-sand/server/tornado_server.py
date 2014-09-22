b5abf5ea0fc845767163576f12c0e0cf037991c0
ransom <auvergnerw@gmail.com>
2014-09-22 15:06:16 -0500

    start owner login functionality

--- text-sand/server/tornado_server.py
+++ text-sand/server/tornado_server.py
@@ -1,13 +1,29 @@
 from os import path
 
+from pdb import set_trace as st
+
 import tornado.web
 
 SCRIPT_DIR = path.dirname(path.realpath(__file__))
 ASSETS_DIR = path.join(path.split(SCRIPT_DIR)[0], 'assets')
 
+SECRET_KEY = 'mmmsecret'
+
 class KeyholeHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("keyhole unimplemented")
+        if not self.get_cookie('owner'):
+            self.render('keyhole.html')
+        else:
+            self.redirect('/peephole')
+
+    def post(self):
+        attempted_key = self.get_body_argument('secret_key')
+        if attempted_key == SECRET_KEY:
+            # set cookie to any truthy value
+            self.set_cookie('owner', 'is_owner')
+            self.redirect('/peephole')
+        else:
+            self.write("got incorrect key '" + attempted_key + "'")
 
 class KnockerHandler(tornado.web.RequestHandler):
     def get(self):
