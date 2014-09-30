c6632e8ec2c713c0bfee0b2a50309e6a7691cabd
Andrew Davis <andrew@lightcorp.net>
2014-09-30 11:07:27 -0500

    added some session handling, fleshing out example structure

--- text-sand/node-server/app.js
+++ text-sand/node-server/app.js
@@ -1,8 +1,22 @@
 var express = require('express');
+var session = require('express-session')
 var app = express();
 
-app.get('/hello', function(req, res){
-  res.send('Hello World!');
+var appState = {
+    guestName: undefined,
+    chatting: false,
+    messages: []
+}
+
+app.get('/peephole', function(req, res, next){
+  if (!req.session.isOwner) res.redirect('/login');
 });
 
+app.get('/', function(req, res, next){
+  if (!req.session || !req.session.isOwner) req.session = { isGuest: true, isowner: false }
+  if (req.session.isOwner) res.redirect('/peephole');
+  else res.redirect('/knocker')
+});
+
+app.use(session({secret:'keyboard cat'}));
 module.exports = app;
